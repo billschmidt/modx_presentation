800ff081d60070f95cce5b75b86037b18ebb1868
Bill Schmidt <wrschmidt.dotcom@gmail.com>
2013-09-22 19:46:48 -0500

    slide transitions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-
+    var effects = ['fade', 'drop', 'puff'];
+    $('body').on('keypress', function(e){
+        var code = (e.keyCode ? e.keyCode : e.which);
+        var cur = $('section.active');
+        if (code == 37){
+            // back
+            var prev = cur.prev('section');
+            if (prev.length == 0) return;
+            window.location.hash = $('section').index(prev);
+        } else if (code == 39){
+            // forward
+            var next = cur.next('section');
+            if (next.length == 0) return;
+            window.location.hash = $('section').index(next);
+        }
+    });
+    $(window).on('hashchange', function(){
+        $('section.active').effect(effects[Math.floor(Math.random() * effects.length)], {mode: 'hide'}, 500, function(){
+            $(this).toggleClass('active');
+            var index = window.location.hash.substr(1);
+            $('section:eq('+index+')').effect('fade', {mode: 'show'}, 500, function(){
+                $(this).toggleClass('active');
+            });
+        });
+    });
 });
